04754d46763b8632cadf0441df72965f6a78ad5f
anasallahham <anastennis12@gmail.com>
2023-07-05 23:17:04 +0300

    Showing Timetables (Students)

--- lib/shared/network/remote/end_points.dart
+++ lib/shared/network/remote/end_points.dart
@@ -1,35 +1,54 @@
 import 'package:untitled/modules/teachers/add/add_teacher.dart';
 
+///REGISTER
 const RegisterStudent='student/register';
 const AdminLogin='login';
+
+///STUDENTS
 const GETSTUDENTS='students/index';
 const GETSTUDENTSBYGRADE='show_by_grade';
 const GETSTUDENTSBYGRADEANDCLASSROOM='show_by_classroomAndGrade';
 
-
+///TEACHER
 const UPDATETEACHER='update_teacher/';
 const ADDTEACHER='teacher/register';
+
+///NOTICES
 const ADDNOTICE='/storenotice';
+
+///SUBJECTS
 const ADDSUBJECT='create_subject';
 const GETSUBJECTS='subjects';
+
+///ADMINS
 const ADDADMIN='admin/register';
+const SHOWADMINS='/admin/index';
+
+///CLASSROOMS
 const ADDCLASSROOM='create_classroom';
 const GETCLASSROOMS='classrooms/index';
-const SHOWADMINS='/admin/index';
 const GETCLASSROOMSOFAGRADE='classrooms/showByGrade';
 
+///SCHOOLYEARS
 const ADDSCHOOLYEAR='create_schoolyear';
 const GETSCHOOLYEARS='schoolyears';
+
+///ATTENDANCE
 const ATTENDANCE='storeattendance';
 const SHOWATTENDANCE='Ashowattendance';
+
+///EXAMS
 const ADDEXAM='exams/store';
 const SHOWEXAMS='exams/show_for_admin';
 
+///TIMETABLES
 const ADDTIMETABLE='create_schedule';
 const SHOWTIMETABLE= 'show_schedule_classroom';
 
+///ABOUT
 const ADDSCHOOL='admin/create_school';
 const SHOWSCHOOL='';
 
+///FILES
 const ADDFILE='upload_file';
 const SHOWFILE ='show_files_classroom';
