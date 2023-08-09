6f707992e329e20b85e1a269aa5113ca4bc44702
anasallahham <anastennis12@gmail.com>
2023-08-10 01:01:53 +0300

    FCM config

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,11 @@ import 'package:e_school/layout/home_layout/home_layout.dart';
 import 'package:e_school/layout/parent_home_layout/parent_home_layout.dart';
 import 'package:e_school/layout/teacher_home_layout/teacher_home_layout.dart';
 import 'package:e_school/modules/profile/profile_screen.dart';
+import 'package:e_school/shared/components/components.dart';
 import 'package:e_school/shared/components/constants.dart';
 import 'package:e_school/shared/cubit/app_cubit.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -22,10 +24,33 @@ import 'modules/on_boarding/on_boarding_screen.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+Future<void> firebaseMessagingBackgroundHandler (RemoteMessage message)async{
+  print('message background');
+  print(message.data.toString());
+  showToast(text: 'message background', state: ToastStates.SUCCESS);
+}
+
+
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp;
+  await Firebase.initializeApp();
+  var deviceToken =await FirebaseMessaging.instance.getToken();
+  print('fcm : ${deviceToken}');
+  FirebaseMessaging.onMessage.listen((event) {
+    print('on message');
+    print(event.data.toString());
+    showToast(text: 'on message', state: ToastStates.SUCCESS);
+
+  });
+  FirebaseMessaging.onMessageOpenedApp.listen((event) {
+    print('on message opened');
+    print(event.data.toString());
+    showToast(text: 'on message opened app', state: ToastStates.SUCCESS);
+  });
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+
+
   Bloc.observer = MyBlocObserver();
   DioHelper.init();
   await dotenv.load(fileName: "assets/.env");
@@ -46,6 +71,7 @@ Future main() async {
         widget = TeacherHomeLayout();
       } else {
         widget = ParentHomeLayout();
+        FirebaseMessaging.instance.subscribeToTopic('parents');
       }
     } else
       widget = SchoolLoginScreen();
@@ -87,6 +113,8 @@ class _MyAppState extends State<MyApp> {
     getLocale().then((locale) => {setLocale(locale)});
     super.didChangeDependencies();
   }
+
+
 
   @override
   Widget build(BuildContext context) {
