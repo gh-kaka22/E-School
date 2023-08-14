8047e12cdd6cf8f2a1118bf798c5abaaeefc40d6
gh_kaka22 <ghassan963988@gmail.com>
2023-08-14 14:46:28 +0300

    python ta7nek

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 
 //
+Route::post('/python-script', [\App\Http\Controllers\PythonController::class , 'answer']);
 
 //............................register routes.............................................
 //Owner routes
